6d3fc8e3c55082e71c158948f5d20100a0210188
WhatEverKid07 <eugeniosanc22@outlook.com>
2024-06-02 16:40:08 +0100

    UI, Buttons

---
+++ Asteroids-Clone/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool gameIsPaused = false;
+    public string mainMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+       
+    void Pause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        gameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
+        gameIsPaused = false;
+        SceneManager.LoadScene(mainMenu);
+    }
+}
