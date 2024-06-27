e3886456bf4b2c60e99c6ec2a71e15817cd11826
WhatEverKid07 <eugeniosanc22@outlook.com>
2024-06-27 19:33:15 +0100

    Ready for arcade

--- Asteroids-Clone/Assets/Scripts/Player.cs
+++ Asteroids-Clone/Assets/Scripts/Player.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private bool player1;
     [SerializeField] private bool player2;
 
-    [Header("Ship Parameteres")]
+    [Header("Ship Parameters")]
     [SerializeField] private float shipAcceleration = 10f;
     [SerializeField] private float shipMaxVelocity = 10f;
     [SerializeField] private float shipRotationSpeed = 180f;
@@ -19,53 +20,82 @@ public class Player : MonoBehaviour
     [SerializeField] private AudioSource shootingSFX;
     [SerializeField] private AudioSource explosionSFX;
 
-    [Header("Object Referance")]
+    [Header("Object Reference")]
     [SerializeField] private Transform bulletSpawn;
     [SerializeField] private Rigidbody2D bulletPrefab;
     [SerializeField] private ParticleSystem destroyedParticles;
 
-    private Rigidbody2D shipRigibody;
+    private Rigidbody2D shipRigidbody;
     private bool isAlive = true;
     private bool isAccelerating = false;
     public GameManager gameManager;
 
+    private PlayerInputActions inputActions;
+    private InputAction moveAction;
+    private InputAction shootAction;
+
+    private void Awake()
+    {
+        shipRigidbody = GetComponent<Rigidbody2D>();
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        if (player1)
+        {
+            moveAction = inputActions.Player1.Move;
+            shootAction = inputActions.Player1.Shoot;
+        }
+        else if (player2)
+        {
+            moveAction = inputActions.Player2.Move;
+            shootAction = inputActions.Player2.Shoot;
+        }
+
+        moveAction.Enable();
+        shootAction.Enable();
+
+        shootAction.performed += ctx => HandleShooting();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        shootAction.Disable();
+    }
+
     private void Start()
     {
-        shipRigibody = GetComponent<Rigidbody2D>();
-        //GameManager gameManager = FindAnyObjectByType<GameManager>();
         gameManager.playersLeft = 2;
     }
+
     private void Update()
     {
-        if (isAlive && player1)
+        if (isAlive)
         {
-            HandleSipAcceleration();
+            HandleShipAcceleration();
             HandleShipRotation();
-            HandleShooting();
-        }
-        if (isAlive && player2)
-        {
-            Player2HandleSipAcceleration();
-            Player2HandleShipRotation();
-            Player2HandleShooting();
         }
     }
+
     private void FixedUpdate()
     {
         if (isAlive && isAccelerating)
         {
-            shipRigibody.AddForce(shipAcceleration * transform.up);
-            shipRigibody.velocity = Vector2.ClampMagnitude(shipRigibody.velocity, shipMaxVelocity);
+            shipRigidbody.AddForce(shipAcceleration * transform.up);
+            shipRigidbody.velocity = Vector2.ClampMagnitude(shipRigidbody.velocity, shipMaxVelocity);
         }
     }
-    private void HandleSipAcceleration()
+
+    private void HandleShipAcceleration()
     {
-        isAccelerating = Input.GetKey(KeyCode.W);
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        isAccelerating = moveInput.y > 0;
         if (isAccelerating && !exhaustParticles.isPlaying)
         {
             exhaustParticles.Play();
             exhaustSFX.Play();
-            Debug.Log("P1 accelerating");
         }
         else if (!isAccelerating && exhaustParticles.isPlaying)
         {
@@ -73,71 +103,24 @@ public class Player : MonoBehaviour
             exhaustSFX.Stop();
         }
     }
-    private void Player2HandleSipAcceleration()
-    {
-        isAccelerating = Input.GetKey(KeyCode.UpArrow);
-        if (isAccelerating && !exhaustParticles.isPlaying)
-        {
-            exhaustParticles.Play();
-            exhaustSFX.Play();
-            Debug.Log("P2 accelerating");
-        }
-        else if (!isAccelerating && exhaustParticles.isPlaying)
-        {
-            exhaustParticles.Stop();
-            exhaustSFX.Stop();
-        }
-    }
+
     private void HandleShipRotation()
     {
-        if (Input.GetKey(KeyCode.A))
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        if (moveInput.x != 0)
         {
-            transform.Rotate(shipRotationSpeed * Time.deltaTime * transform.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-shipRotationSpeed * Time.deltaTime * transform.forward);
+            transform.Rotate(-moveInput.x * shipRotationSpeed * Time.deltaTime * Vector3.forward);
         }
     }
-    private void Player2HandleShipRotation()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(shipRotationSpeed * Time.deltaTime * transform.forward);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(-shipRotationSpeed * Time.deltaTime * transform.forward);
-        }
-    }
+
     private void HandleShooting()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isAlive)
         {
             shootingSFX.Play();
             Rigidbody2D bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
 
-            Vector2 shipVelocity = shipRigibody.velocity;
-            Vector2 shipDirection = transform.up;
-            float shipForwardSpeed = Vector2.Dot(shipVelocity, shipDirection);
-
-            if(shipForwardSpeed < 0)
-            {
-                shipForwardSpeed = 0;
-            }
-            
-            bullet.velocity = shipDirection * shipForwardSpeed;
-            bullet.AddForce(bulletSpeed * transform.up, ForceMode2D.Impulse);
-        }
-    }
-    private void Player2HandleShooting()
-    {
-        if (Input.GetKeyDown(KeyCode.RightControl))
-        {
-            shootingSFX.Play();
-            Rigidbody2D bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
-
-            Vector2 shipVelocity = shipRigibody.velocity;
+            Vector2 shipVelocity = shipRigidbody.velocity;
             Vector2 shipDirection = transform.up;
             float shipForwardSpeed = Vector2.Dot(shipVelocity, shipDirection);
 
@@ -150,18 +133,18 @@ public class Player : MonoBehaviour
             bullet.AddForce(bulletSpeed * transform.up, ForceMode2D.Impulse);
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Asteroids"))
         {
             explosionSFX.Play();
             isAlive = false;
-            //GameManager gameManager = FindAnyObjectByType<GameManager>();
-            gameManager.playersLeft-= 1;
+            gameManager.playersLeft -= 1;
             Instantiate(destroyedParticles, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
-        if(gameManager.playersLeft == 0)
+        if (gameManager.playersLeft == 0)
         {
             gameManager.GameOver();
         }
--- Asteroids-Clone/Assets/Scripts/PlayerOne.cs
+++ Asteroids-Clone/Assets/Scripts/PlayerOne.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerOne : MonoBehaviour
 {
-    [Header("Ship Parameteres")]
+    [Header("Ship Parameters")]
     [SerializeField] private float shipAcceleration = 10f;
     [SerializeField] private float shipMaxVelocity = 10f;
     [SerializeField] private float shipRotationSpeed = 180f;
@@ -16,41 +17,65 @@ public class PlayerOne : MonoBehaviour
     [SerializeField] private AudioSource shootingSFX;
     [SerializeField] private AudioSource explosionSFX;
 
-    [Header("Object Referance")]
+    [Header("Object Reference")]
     [SerializeField] private Transform bulletSpawn;
     [SerializeField] private Rigidbody2D bulletPrefab;
     [SerializeField] private ParticleSystem destroyedParticles;
 
-    private Rigidbody2D shipRigibody;
+    private Rigidbody2D shipRigidbody;
     private bool isAlive = true;
     private bool isAccelerating = false;
     public GameManager gameManager;
 
-    private void Start()
+    private PlayerInputActions inputActions;
+    private InputAction moveAction;
+    private InputAction shootAction;
+
+    private void Awake()
     {
-        shipRigibody = GetComponent<Rigidbody2D>();
-        //GameManager gameManager = FindAnyObjectByType<GameManager>();
+        shipRigidbody = GetComponent<Rigidbody2D>();
+        inputActions = new PlayerInputActions();
     }
+
+    private void OnEnable()
+    {
+        moveAction = inputActions.Player1.Move;
+        shootAction = inputActions.Player1.Shoot;
+
+        moveAction.Enable();
+        shootAction.Enable();
+
+        shootAction.performed += ctx => HandleShooting();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        shootAction.Disable();
+    }
+
     private void Update()
     {
         if (isAlive)
         {
-            HandleSipAcceleration();
+            HandleShipAcceleration();
             HandleShipRotation();
-            HandleShooting();
         }
     }
+
     private void FixedUpdate()
     {
         if (isAlive && isAccelerating)
         {
-            shipRigibody.AddForce(shipAcceleration * transform.up);
-            shipRigibody.velocity = Vector2.ClampMagnitude(shipRigibody.velocity, shipMaxVelocity);
+            shipRigidbody.AddForce(shipAcceleration * transform.up);
+            shipRigidbody.velocity = Vector2.ClampMagnitude(shipRigidbody.velocity, shipMaxVelocity);
         }
     }
-    private void HandleSipAcceleration()
+
+    private void HandleShipAcceleration()
     {
-        isAccelerating = Input.GetKey(KeyCode.W);
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        isAccelerating = moveInput.y > 0;
         if (isAccelerating && !exhaustParticles.isPlaying)
         {
             exhaustParticles.Play();
@@ -63,45 +88,44 @@ public class PlayerOne : MonoBehaviour
             exhaustSFX.Stop();
         }
     }
+
     private void HandleShipRotation()
     {
-        if (Input.GetKey(KeyCode.A))
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        if (moveInput.x != 0)
         {
-            transform.Rotate(shipRotationSpeed * Time.deltaTime * transform.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-shipRotationSpeed * Time.deltaTime * transform.forward);
+            transform.Rotate(-moveInput.x * shipRotationSpeed * Time.deltaTime * Vector3.forward);
         }
     }
 
     private void HandleShooting()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isAlive)
         {
             shootingSFX.Play();
             Rigidbody2D bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
 
-            Vector2 shipVelocity = shipRigibody.velocity;
+            Vector2 shipVelocity = shipRigidbody.velocity;
             Vector2 shipDirection = transform.up;
             float shipForwardSpeed = Vector2.Dot(shipVelocity, shipDirection);
 
-            if(shipForwardSpeed < 0)
+            if (shipForwardSpeed < 0)
             {
                 shipForwardSpeed = 0;
             }
-            
+
             bullet.velocity = shipDirection * shipForwardSpeed;
             bullet.AddForce(bulletSpeed * transform.up, ForceMode2D.Impulse);
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Asteroids"))
         {
             explosionSFX.Play();
             isAlive = false;
-            //GameManager gameManager = FindAnyObjectByType<GameManager>();
+            // GameManager gameManager = FindAnyObjectByType<GameManager>();
             gameManager.GameOver();
             Instantiate(destroyedParticles, transform.position, Quaternion.identity);
             Destroy(gameObject);
